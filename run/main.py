0a6300d62b5ccfe61b0322b368332bf352ecc2b7
LordBramster <49885100+LordBramster@users.noreply.github.com>
2022-07-16 21:38:21 -0600

    Update main.py

--- run/main.py
+++ run/main.py
@@ -21,18 +21,84 @@ class App(ttk.Frame):
         self.var_enter_password = tk.StringVar(value='')
         self.var_progressbar = tk.DoubleVar(value=0.0)
 
-        self.setup_windows()
+        self.setup_start_windows()
 
-    def setup_windows(self):
+    def setup_start_windows(self):
         self.setup_login()
         # self.setup_main_menu()
         # self.setup_new_window()
 
-    def setup_new_window(self):
+    def setup_next_window(self, name):
+        self.clear_frame(self)
+        if name == 'MAIN': self.setup_main_menu()
+        if name == 'LOGIN': self.setup_login()
+        if name == 'VIEW': self.setup_viewer()
+
+    def setup_create_account(self):
         new_window = tk.Toplevel(self.parent)
-        new_window.geometry("500x500")
+        # new_window.geometry("500x500")
         new_window.iconbitmap("gui/locked.ico")
         new_window.title("Sign Up")
+
+        frame_root = ttk.Frame(new_window, padding=(0, 0, 0, 10))
+        frame_root.grid(row=0, column=0, padx=(30, 30), pady=(20, 50), sticky="nsew", rowspan=3)
+        frame_root.columnconfigure(index=0, weight=1)
+
+        label = ttk.Label(
+            frame_root,
+            text="Sign Up",
+            justify="center",
+            font=("-size", 30, "-weight", "bold"))
+        label.grid(row=1, column=0, pady=10, columnspan=2)
+
+        frame_account = ttk.Frame(frame_root, padding=(0, 0, 0, 0))
+        frame_account.grid(row=10, column=0, padx=(1, 1), pady=(5, 5), sticky="nsew", rowspan=3)
+        frame_account.columnconfigure(index=0, weight=1)
+
+        label_account = ttk.Label(
+            frame_account,
+            text="Enter account name :",
+            justify="left",
+            font=("-size", 8, "-weight", "bold"))
+        label_account.grid(row=0, column=0, pady=10, columnspan=2, sticky="w")
+
+        entry_name = ttk.Entry(frame_account)
+        entry_name.insert(0, self.var_enter_username.get())
+        entry_name.grid(row=1, column=0, padx=(1, 1), pady=(1, 1), ipady=10, ipadx=60, sticky="ew")
+
+        frame_pass = ttk.Frame(frame_root, padding=(0, 0, 0, 0))
+        frame_pass.grid(row=20, column=0, padx=(1, 1), pady=(5, 5), sticky="nsew", rowspan=3)
+        frame_pass.columnconfigure(index=0, weight=1)
+
+        label_pass = ttk.Label(
+            frame_pass,
+            text="Enter a password :",
+            justify="left",
+            font=("-size", 8, "-weight", "bold"))
+        label_pass.grid(row=0, column=0, pady=10, columnspan=2, sticky="w")
+
+        entry_pass = ttk.Entry(frame_pass, show='*')
+        entry_pass.insert(0, self.var_enter_password.get())
+        entry_pass.grid(row=1, column=0, padx=(1, 1), pady=(1, 1), ipady=10, ipadx=60, sticky="ew")
+
+        frame_confirm = ttk.Frame(frame_root, padding=(0, 0, 0, 0))
+        frame_confirm.grid(row=30, column=0, padx=(1, 1), pady=(5, 5), sticky="nsew", rowspan=3)
+        frame_confirm.columnconfigure(index=0, weight=1)
+
+        label_confirm = ttk.Label(
+            frame_confirm,
+            text="Re-enter your password :",
+            justify="left",
+            font=("-size", 8, "-weight", "bold"))
+        label_confirm.grid(row=0, column=0, pady=10, columnspan=2, sticky="w")
+
+        entry_pass_confirm = ttk.Entry(frame_confirm, show='*')
+        entry_pass_confirm.insert(0, self.var_enter_password.get())
+        entry_pass_confirm.grid(row=1, column=0, padx=(1, 1), pady=(1, 1), ipady=10, ipadx=60, sticky="ew")
+
+        button = ttk.Button(frame_root, text="Create Account",
+                            style="Accent.TButton")
+        button.grid(row=40, column=0, padx=(1, 1), pady=(10, 1), ipady=10, ipadx=30, sticky="ew")
 
     def setup_login(self):
         frame_root = ttk.Frame(self, padding=(0, 0, 0, 10))
@@ -59,16 +125,16 @@ class App(ttk.Frame):
 
         entry_name = ttk.Entry(frame_root)
         entry_name.insert(0, self.var_enter_username.get())
-        entry_name.grid(row=4, column=0, padx=(1, 1), pady=(30, 1), ipady=10, ipadx=40, sticky="ew")
+        entry_name.grid(row=4, column=0, padx=(1, 1), pady=(10, 1), ipady=10, ipadx=40, sticky="ew")
 
         entry_pass = ttk.Entry(frame_root, show='*')
-        entry_pass.insert(0, self.var_enter_username.get())
-        entry_pass.grid(row=5, column=0, padx=(1, 1), pady=(1, 30), ipady=10, ipadx=40, sticky="ew")
+        entry_pass.insert(0, self.var_enter_password.get())
+        entry_pass.grid(row=5, column=0, padx=(1, 1), pady=(10, 1), ipady=10, ipadx=40, sticky="ew")
 
         button = ttk.Button(frame_root, text="Sign In",
                             style="Accent.TButton",
-                            command=lambda: self.setup_main_menu())
-        button.grid(row=6, column=0, padx=(1, 1), pady=(1, 1), ipady=10, ipadx=40, sticky="ew")
+                            command=lambda: self.setup_next_window('VIEW'))
+        button.grid(row=6, column=0, padx=(1, 1), pady=(30, 1), ipady=10, ipadx=40, sticky="ew")
 
         label3 = ttk.Label(
             frame_root,
@@ -79,7 +145,7 @@ class App(ttk.Frame):
 
         button = ttk.Button(frame_root, text="Create Account",
                             # style="Accent.TButton",
-                            command=lambda: self.setup_new_window())
+                            command=lambda: self.setup_create_account())
         button.grid(row=8, column=0, padx=(1, 1), pady=(1, 1), ipady=10, ipadx=20, sticky="ew")
 
         """     
@@ -87,41 +153,7 @@ class App(ttk.Frame):
         progressbar.grid(row=10, column=0, padx=(10, 20), pady=(20, 0), sticky="ew")
         """
 
-    def setup_create_account(self):
-        return
-
-    def setup_main_menu(self):
-        self.img = tk.PhotoImage(file='gui/locked_003.png')
-        self.img_b = tk.Label(self, image=self.img)
-        self.img_b.grid(row=0, column=0, padx=(40, 40), pady=(20, 10), sticky="ew")
-
-        self.frame_L = ttk.Frame(self, padding=(0, 0, 0, 10))
-        self.frame_L.grid(
-            row=1, column=0, padx=10, pady=(30, 10), sticky="nsew", rowspan=3)
-        self.frame_L.columnconfigure(index=0, weight=1)
-
-        # Button
-        self.button = ttk.Button(self.frame_L, text="Edit Credentials")
-        self.button.grid(row=6, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
-
-        # Button
-        self.button2 = ttk.Button(self.frame_L, text="Copy Credentials")
-        self.button2.grid(row=7, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
-
-        # Button
-        self.button3 = ttk.Button(self.frame_L, text="Export (as .PDF, .DOCX, or .TXT)")
-        self.button3.grid(row=8, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
-
-        # Accentbutton
-        self.accentbutton = ttk.Button(
-            self.frame_L, text="Import Credentials", style="Accent.TButton", command=lambda: ask_openfile())
-        self.accentbutton.grid(row=9, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
-
-        # Accentbutton
-        self.accentbutton2 = ttk.Button(
-            self.frame_L, text="Add New Credentials", style="Accent.TButton")
-        self.accentbutton2.grid(row=10, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
-
+    def setup_viewer(self):
         # Panedwindow
         self.paned = ttk.PanedWindow(self)
         self.paned.grid(row=0, column=2, pady=(25, 5), sticky="nsew", rowspan=3)
@@ -184,9 +216,46 @@ class App(ttk.Frame):
         self.sizegrip = ttk.Sizegrip(self)
         self.sizegrip.grid(row=100, column=100, padx=(0, 5), pady=(0, 5))
 
+    def setup_main_menu(self):
+        self.img = tk.PhotoImage(file='gui/locked_003.png')
+        self.img_b = tk.Label(self, image=self.img)
+        self.img_b.grid(row=0, column=0, padx=(40, 40), pady=(20, 10), sticky="ew")
+
+        self.frame_L = ttk.Frame(self, padding=(0, 0, 0, 10))
+        self.frame_L.grid(
+            row=1, column=0, padx=10, pady=(30, 10), sticky="nsew", rowspan=3)
+        self.frame_L.columnconfigure(index=0, weight=1)
+
+        # Button
+        self.button = ttk.Button(self.frame_L, text="Edit Credentials")
+        self.button.grid(row=6, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
+
+        # Button
+        self.button2 = ttk.Button(self.frame_L, text="Copy Credentials")
+        self.button2.grid(row=7, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
+
+        # Button
+        self.button3 = ttk.Button(self.frame_L, text="Export (as .PDF, .DOCX, or .TXT)")
+        self.button3.grid(row=8, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
+
+        # Accentbutton
+        self.accentbutton = ttk.Button(
+            self.frame_L, text="Import Credentials", style="Accent.TButton", command=lambda: ask_openfile())
+        self.accentbutton.grid(row=9, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
+
+        # Accentbutton
+        self.accentbutton2 = ttk.Button(
+            self.frame_L, text="Add New Credentials", style="Accent.TButton")
+        self.accentbutton2.grid(row=10, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
+
     def click_callback(self, event, widget):
         widget.delete(0, "end")
         return None
+
+    def clear_frame(self, frame):
+        # destroy all widgets from frame
+        for widget in frame.winfo_children():
+            widget.destroy()
 
     def progressbar_increase(self, number=15):
         self.var_progressbar.set(self.var_progressbar.get() + number)
