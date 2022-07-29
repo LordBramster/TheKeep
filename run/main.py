41e30d1e77b7f5ae42520dd5b6c3a218a38c658b
LordBramster <49885100+LordBramster@users.noreply.github.com>
2022-07-28 21:20:45 -0600

    Update main.py

--- run/main.py
+++ run/main.py
@@ -3,8 +3,10 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 import json
+import yaml
 from cryptography.fernet import Fernet
 import pyperclip
+import random
 import time
 
 
@@ -19,6 +21,9 @@ class App(ttk.Frame):
 
         self.var_enter_username = tk.StringVar(value='Account Name')
         self.var_enter_password = tk.StringVar(value='')
+        self.var_new_username = tk.StringVar(value='')
+        self.var_new_password = tk.StringVar(value='')
+        self.var_new_password_confirm = tk.StringVar(value='')
         self.var_progressbar = tk.DoubleVar(value=0.0)
 
         self.setup_start_windows()
@@ -34,13 +39,18 @@ class App(ttk.Frame):
         if name == 'LOGIN': self.setup_login()
         if name == 'VIEW': self.setup_viewer()
 
-    def setup_create_account(self):
-        new_window = tk.Toplevel(self.parent)
-        # new_window.geometry("500x500")
-        new_window.iconbitmap("gui/locked.ico")
-        new_window.title("Sign Up")
+    def setup_next_created(self, name):
+        self.new_window.destroy()
+        generate_uid(UID_STR, UID_DIR)
+        self.setup_next_window(name)
 
-        frame_root = ttk.Frame(new_window, padding=(0, 0, 0, 10))
+    def setup_create_account(self):
+        self.new_window = tk.Toplevel(self.parent)
+        # new_window.geometry("500x500")
+        self.new_window.iconbitmap("gui/locked.ico")
+        self.new_window.title("Sign Up")
+
+        frame_root = ttk.LabelFrame(self.new_window, padding=(0, 0, 0, 10))
         frame_root.grid(row=0, column=0, padx=(30, 30), pady=(20, 50), sticky="nsew", rowspan=3)
         frame_root.columnconfigure(index=0, weight=1)
 
@@ -51,7 +61,7 @@ class App(ttk.Frame):
             font=("-size", 30, "-weight", "bold"))
         label.grid(row=1, column=0, pady=10, columnspan=2)
 
-        frame_account = ttk.Frame(frame_root, padding=(0, 0, 0, 0))
+        frame_account = ttk.LabelFrame(frame_root, padding=(0, 0, 0, 0))
         frame_account.grid(row=10, column=0, padx=(1, 1), pady=(5, 5), sticky="nsew", rowspan=3)
         frame_account.columnconfigure(index=0, weight=1)
 
@@ -63,10 +73,10 @@ class App(ttk.Frame):
         label_account.grid(row=0, column=0, pady=10, columnspan=2, sticky="w")
 
         entry_name = ttk.Entry(frame_account)
-        entry_name.insert(0, self.var_enter_username.get())
+        entry_name.insert(0, self.var_new_username.get())
         entry_name.grid(row=1, column=0, padx=(1, 1), pady=(1, 1), ipady=10, ipadx=60, sticky="ew")
 
-        frame_pass = ttk.Frame(frame_root, padding=(0, 0, 0, 0))
+        frame_pass = ttk.LabelFrame(frame_root, padding=(0, 0, 0, 0))
         frame_pass.grid(row=20, column=0, padx=(1, 1), pady=(5, 5), sticky="nsew", rowspan=3)
         frame_pass.columnconfigure(index=0, weight=1)
 
@@ -78,10 +88,10 @@ class App(ttk.Frame):
         label_pass.grid(row=0, column=0, pady=10, columnspan=2, sticky="w")
 
         entry_pass = ttk.Entry(frame_pass, show='*')
-        entry_pass.insert(0, self.var_enter_password.get())
+        entry_pass.insert(0, self.var_new_password.get())
         entry_pass.grid(row=1, column=0, padx=(1, 1), pady=(1, 1), ipady=10, ipadx=60, sticky="ew")
 
-        frame_confirm = ttk.Frame(frame_root, padding=(0, 0, 0, 0))
+        frame_confirm = ttk.LabelFrame(frame_root, padding=(0, 0, 0, 0))
         frame_confirm.grid(row=30, column=0, padx=(1, 1), pady=(5, 5), sticky="nsew", rowspan=3)
         frame_confirm.columnconfigure(index=0, weight=1)
 
@@ -93,15 +103,16 @@ class App(ttk.Frame):
         label_confirm.grid(row=0, column=0, pady=10, columnspan=2, sticky="w")
 
         entry_pass_confirm = ttk.Entry(frame_confirm, show='*')
-        entry_pass_confirm.insert(0, self.var_enter_password.get())
+        entry_pass_confirm.insert(0, self.var_new_password_confirm.get())
         entry_pass_confirm.grid(row=1, column=0, padx=(1, 1), pady=(1, 1), ipady=10, ipadx=60, sticky="ew")
 
         button = ttk.Button(frame_root, text="Create Account",
-                            style="Accent.TButton")
+                            style="Accent.TButton",
+                            command=lambda: self.setup_next_created('MAIN'))
         button.grid(row=40, column=0, padx=(1, 1), pady=(10, 1), ipady=10, ipadx=30, sticky="ew")
 
     def setup_login(self):
-        frame_root = ttk.Frame(self, padding=(0, 0, 0, 10))
+        frame_root = ttk.LabelFrame(self, padding=(0, 0, 0, 10))
         frame_root.grid(row=0, column=0, padx=(30, 30), pady=(50, 50), sticky="nsew", rowspan=3)
         frame_root.columnconfigure(index=0, weight=1)
 
@@ -133,7 +144,7 @@ class App(ttk.Frame):
 
         button = ttk.Button(frame_root, text="Sign In",
                             style="Accent.TButton",
-                            command=lambda: self.setup_next_window('VIEW'))
+                            command=lambda: self.setup_next_window('MAIN'))
         button.grid(row=6, column=0, padx=(1, 1), pady=(30, 1), ipady=10, ipadx=40, sticky="ew")
 
         label3 = ttk.Label(
@@ -188,7 +199,7 @@ class App(ttk.Frame):
         self.treeview.heading(2, text="Password", anchor="center")
         self.treeview.heading(3, text="2FA-Recovery", anchor="center")
 
-        # Define treeview data
+        # Define treeview users
         treeview_data = []
         p = 0
         for parent, value1 in DISPLAY_DATA.items():
@@ -204,7 +215,7 @@ class App(ttk.Frame):
                 )
             p = c
 
-        # Insert treeview data
+        # Insert treeview users
         for item in treeview_data:
             self.treeview.insert(
                 parent=item[0], index="end", iid=item[1], text=item[2], values=item[3]
@@ -221,39 +232,38 @@ class App(ttk.Frame):
         self.img_b = tk.Label(self, image=self.img)
         self.img_b.grid(row=0, column=0, padx=(40, 40), pady=(20, 10), sticky="ew")
 
-        self.frame_L = ttk.Frame(self, padding=(0, 0, 0, 10))
+        self.frame_L = ttk.LabelFrame(self, padding=(0, 0, 0, 10))
         self.frame_L.grid(
             row=1, column=0, padx=10, pady=(30, 10), sticky="nsew", rowspan=3)
         self.frame_L.columnconfigure(index=0, weight=1)
 
         # Button
         self.button = ttk.Button(self.frame_L, text="Edit Credentials")
-        self.button.grid(row=6, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
+        self.button.grid(row=6, column=0, padx=5, pady=10, ipadx=5, ipady=10, sticky="nsew")
 
         # Button
         self.button2 = ttk.Button(self.frame_L, text="Copy Credentials")
-        self.button2.grid(row=7, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
+        self.button2.grid(row=7, column=0, padx=5, pady=10, ipadx=5, ipady=10, sticky="nsew")
 
         # Button
         self.button3 = ttk.Button(self.frame_L, text="Export (as .PDF, .DOCX, or .TXT)")
-        self.button3.grid(row=8, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
+        self.button3.grid(row=8, column=0, padx=5, pady=10, ipadx=5, ipady=10, sticky="nsew")
 
         # Accentbutton
         self.accentbutton = ttk.Button(
             self.frame_L, text="Import Credentials", style="Accent.TButton", command=lambda: ask_openfile())
-        self.accentbutton.grid(row=9, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
+        self.accentbutton.grid(row=9, column=0, padx=5, pady=10, ipadx=5, ipady=10, sticky="nsew")
 
         # Accentbutton
         self.accentbutton2 = ttk.Button(
             self.frame_L, text="Add New Credentials", style="Accent.TButton")
-        self.accentbutton2.grid(row=10, column=0, padx=5, pady=10, ipadx=1, ipady=5, sticky="nsew")
+        self.accentbutton2.grid(row=10, column=0, padx=5, pady=10, ipadx=5, ipady=10, sticky="nsew")
 
     def click_callback(self, event, widget):
         widget.delete(0, "end")
         return None
 
     def clear_frame(self, frame):
-        # destroy all widgets from frame
         for widget in frame.winfo_children():
             widget.destroy()
 
@@ -261,7 +271,7 @@ class App(ttk.Frame):
         self.var_progressbar.set(self.var_progressbar.get() + number)
 
 
-def clipboard_copy(self, item):
+def clipboard_copy(item):
     pyperclip.copy(item)
 
 
@@ -294,6 +304,10 @@ def file_reads(file):
     return data
 
 
+def folder_content(path):
+    return os.listdir(path)
+
+
 def encrypt(string, key):
     return Fernet(key).encrypt(string.encode())
 
@@ -304,6 +318,11 @@ def decrypt(string, key):
 
 def generate_key():
     return Fernet.generate_key()
+
+
+def generate_uid(prefix, uid_dir):
+    new_uid = f'{prefix}{random.randrange(0, 99999999)}'
+    return new_uid if new_uid not in folder_content(uid_dir) else None
 
 
 def start_gui():
@@ -353,10 +372,13 @@ def push_data_to(from_data, dump_file, key_file):
 if __name__ == "__main__":
     FILES_ACCEPT = [('Custom Files', '*.ky')]
     FILE_BLANK = 'bucket/JSON/blank.json'
-    FILE_DUMP_KEY = f'bucket/data/k_dump.ky'
-    FILE_DUMP_FILE = f'bucket/data/j_dump.ky'
+    FILE_DUMP_KEY = f'bucket/users/k_dump.ky'
+    FILE_DUMP_FILE = f'bucket/users/j_dump.ky'
     DISPLAY_DATA = get_data_from(FILE_DUMP_FILE, FILE_DUMP_KEY)
     # SET_BLANK_DATA = load_json(FILE_BLANK)
+    UID_ACTIVE = None
+    UID_STR = 'uid-'
+    UID_DIR = './bucket/users/'
 
     # START GUI
     start_gui()
